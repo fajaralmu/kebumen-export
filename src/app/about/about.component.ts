0ede2498710a4538dff13e55c5c4d7701f5eb113
Fajar AM <fajar.munawar@m2.formulatrix.com>
2021-12-15 14:24:37 +0700

    fix bg image, catalog label, contact page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  phoneNumber:string  = "6287738539095";
+  email:string        = "somabangsa@gmail.com";
+  get contactUrl()
+  {
+    const url:string = `https://api.whatsapp.com/send?phone=${this.phoneNumber}&text=Hallo%20Admin%20UniversalHandmade%0ASaya%20ingin%20bertanya%20tentang%20produk%20anda%20.`;
+    return url;
+  }
   constructor() { }
 
   ngOnInit(): void {
