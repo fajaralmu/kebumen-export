7769b749e789def986ee3044296bffea53c3147b
Fajar AM <fajar.munawar@m2.formulatrix.com>
2021-12-16 11:30:12 +0700

    make 1 view only

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'div[app-root]',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Universal Handmade'.toUpperCase();
+
+  scrollTo(id:string)
+  {
+    document.getElementById(id)?.scrollIntoView({behavior: 'smooth'});
+  }
 }
--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -7,9 +7,22 @@ const DEFAULT_RESULT_WIDTH:number = 600;
 })
 export class IndexComponent implements OnInit {
 
+  get motto():string 
+  {
+    return "Company motto".toUpperCase();
+  }
+  description:string = "Kabupetan Kabumen memiliki sejarah tersendiri yaitu berdiri Kabupaten Kebumen dimana maksud yang dikandung untuk memberikan rasa bangga dan memiliki bagi warga masyarakat Kabupaten Kebumen yang selanjutnya dapat menumbuh kembangkan potensi-potensi yang ada sehingga dapat memajukan pembangunan di segala bidang";
 
   ngOnInit(): void { }
 
-    
+  scrollToProduct()
+  {
+    document.getElementById('product_view')?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  scrollToContact()
+  {
+    document.getElementById('about_view')?.scrollIntoView({ behavior: 'smooth' });
+  }
 
 }
