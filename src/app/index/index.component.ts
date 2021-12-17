b60dd97827adeb465f299457ad4766a1816290bc
Fajar AM <fajar.munawar@m2.formulatrix.com>
2021-12-17 10:14:01 +0700

    Relayout

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -7,11 +7,15 @@ const DEFAULT_RESULT_WIDTH:number = 600;
 })
 export class IndexComponent implements OnInit {
 
-  get motto():string 
-  {
-    return "Company motto".toUpperCase();
-  }
+  descriptions:string[] = [
+    "More than 50 professional's craftmans",
+    "Distributed to many cities",
+    "Trusted quality",
+    "Customizable shape, size, and materials"
+  ];
   description:string = "Kabupetan Kabumen memiliki sejarah tersendiri yaitu berdiri Kabupaten Kebumen dimana maksud yang dikandung untuk memberikan rasa bangga dan memiliki bagi warga masyarakat Kabupaten Kebumen yang selanjutnya dapat menumbuh kembangkan potensi-potensi yang ada sehingga dapat memajukan pembangunan di segala bidang";
+  imageUrl:string = "assets/images/products/wooden-craft2.jpg";
+
 
   ngOnInit(): void { }
 
